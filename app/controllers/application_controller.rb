419f26b9e593423c1a4f43a5fb3513e9b8456e95
Catherine Salazar <catcreates13@gmail.com>
2018-10-30 01:33:33 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,10 +37,10 @@ class ApplicationController < Sinatra::Base
      redirect to "/recipes/#{@recipe.id}"
    end
    
-  post '/recipes' do
-    @recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
-    redirect to "recipes/#{@recipe.id}"
-  end
+  # post '/recipes' do
+  #   @recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+  #   redirect to "recipes/#{@recipe.id}"
+  # end
 
 
 
