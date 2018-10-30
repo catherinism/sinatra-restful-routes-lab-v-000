cbf925db12bd673654ec408ec2917723813fbd31
Catherine Salazar <catcreates13@gmail.com>
2018-10-30 01:32:41 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,12 +16,12 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
-  #show action
-    get '/recipes/:id' do
-      @recipe = Recipe.find_by_id(params[:id])
-      erb :show
-    end
-    
+#show action
+  get '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :show
+ end
+
   post '/recipes' do
     @recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
     redirect to "recipes/#{@recipe.id}"
