3afb3074188a72605818e8cb485a4795e39c79e4
Catherine Salazar <catcreates13@gmail.com>
2018-10-30 00:51:11 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,7 +39,7 @@ class ApplicationController < Sinatra::Base
     @recipe.ingredients = params[:ingredients]
     @recipe.cook_time = params[:cook_time]
     @recipe.save
-    erb :show
+    redirect to "/recipes/show"
   end
 
 
