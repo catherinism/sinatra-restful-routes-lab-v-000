35f352981752835b0eb953223a3f3c3693d9ab8a
Catherine Salazar <catcreates13@gmail.com>
2018-10-30 01:33:18 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,27 +22,27 @@ class ApplicationController < Sinatra::Base
     erb :show
  end
 
+ #edit action
+   get '/recipes/:id/edit' do #edit form
+     @recipe = Recipe.find_by_id(params[:id])
+     erb :edit
+   end
+
+   patch 'recipes/:id' do #edit action
+     @recipe = Recipe.find_by_id(params[:id])
+     @recipe.name = params[:name]
+     @recipe.ingredients = params[:ingredients]
+     @recipe.cook_time = params[:cook_time]
+     @recipe.save
+     redirect to "/recipes/#{@recipe.id}"
+   end
+   
   post '/recipes' do
     @recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
     redirect to "recipes/#{@recipe.id}"
   end
 
 
-
-#edit action
-  get '/recipes/:id/edit' do #edit form
-    @recipe = Recipe.find_by_id(params[:id])
-    erb :edit
-  end
-
-  patch 'recipes/:id' do #edit action
-    @recipe = Recipe.find_by_id(params[:id])
-    @recipe.name = params[:name]
-    @recipe.ingredients = params[:ingredients]
-    @recipe.cook_time = params[:cook_time]
-    @recipe.save
-    redirect to "/recipes/#{@recipe.id}"
-  end
 
   #delete action
 
